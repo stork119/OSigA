97c940ef2e446c897e56a65f2a2373ad98d51ba4
stork119 <karol.nienaltowski@gmail.com>
2017-06-02 11:18:57 +0200

    Unified loading rcpp function in different os

--- R/jakstat_model.R
+++ R/jakstat_model.R
@@ -3,8 +3,16 @@
 ### ###
 
 
-PKG_CXXFLAGS="-I/home/knt/Programs/Sundials/cvodes-2.9.0-inst/include -L/home/knt/Programs/Sundials/cvodes-2.9.0-inst/lib -mtune=core2 -std=c++11"
-PKG_LIBS="-I/home/knt/Programs/Sundials/cvodes-2.9.0-inst/include -L/home/knt/Programs/Sundials/cvodes-2.9.0-inst/lib  -lsundials_cvodes -lsundials_nvecserial -Wl,-rpath,/home/knt/Programs/Sundials/cvodes-2.9.0-inst/lib -pthread"
+if( Sys.info()[["sysname"]] == "Linux"){
+  PKG_CXXFLAGS="-I/home/knt/Programs/Sundials/cvodes-2.9.0-inst/include -L/home/knt/Programs/Sundials/cvodes-2.9.0-inst/lib -mtune=core2 -std=c++11"
+  PKG_LIBS="-I/home/knt/Programs/Sundials/cvodes-2.9.0-inst/include -L/home/knt/Programs/Sundials/cvodes-2.9.0-inst/lib  -lsundials_cvodes -lsundials_nvecserial -Wl,-rpath,/home/knt/Programs/Sundials/cvodes-2.9.0-inst/lib -pthread"
+} else if(Sys.info()[["sysname"]] == "Windows" ){
+  print("NOT WORKING !!!")
+  break()
+} else {
+  PKG_CXXFLAGS="-I/usr/local/include -I/usr/local/include/cvode -I/usr/local/include/cvodes -L/usr/local/lib  -mtune=core2 -std=c++11"
+  PKG_LIBS="-I/usr/local/include -I/usr/local/include/cvode -I/usr/local/include/cvodes -L/usr/local/lib  -lsundials_cvodes -lsundials_nvecserial -Wl,-rpath,/usr/local/lib -pthread"
+}
 
 Sys.setenv("PKG_CXXFLAGS" = PKG_CXXFLAGS)
 Sys.setenv("PKG_LIBS" = PKG_LIBS)
