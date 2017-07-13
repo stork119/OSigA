80ec112c0808b349d3633d27614d2f79969c73f7
stork119 <karol.nienaltowski@gmail.com>
2017-07-13 10:08:24 +0200

    Compliation procedure

--- R/jakstat_model.R
+++ R/jakstat_model.R
@@ -4,8 +4,13 @@
 
 
 if( Sys.info()[["sysname"]] == "Linux"){
-  PKG_CXXFLAGS="-I/home/knt/Programs/Sundials/cvodes-2.9.0-inst/include -L/home/knt/Programs/Sundials/cvodes-2.9.0-inst/lib -mtune=core2 -std=c++11"
-  PKG_LIBS="-I/home/knt/Programs/Sundials/cvodes-2.9.0-inst/include -L/home/knt/Programs/Sundials/cvodes-2.9.0-inst/lib  -lsundials_cvodes -lsundials_nvecserial -Wl,-rpath,/home/knt/Programs/Sundials/cvodes-2.9.0-inst/lib -pthread"
+  if(Sys.info()[["nodename"]]){
+    PKG_CXXFLAGS="-I/usr/share/sundials/sundials-2.7.0/instdir/include -I/usr/share/sundials/sundials-2.7.0/instdir/include/cvode -I/usr/share/sundials/sundials-2.7.0/instdir/include/cvodes -L/usr/share/sundials/sundials-2.7.0/instdir/lib -mtune=core2 -std=c++11      -Wl,-rpath,/usr/share/sundials/sundials-2.7.0/instdir/lib"
+    PKG_LIBS="-I/usr/share/sundials/sundials-2.7.0/instdir/include -L/usr/share/sundials/sundials-2.7.0/instdir/lib  -lsundials_cvodes -lsundials_nvecserial -Wl,-rpath,/home/knt/Programs/Sundials/cvodes-2.9.0-inst/lib -pthread      -Wl,-rpath,/usr/share/sundials/sundials-2.7.0/instdir/lib"
+  } else {
+    PKG_CXXFLAGS="-I/home/knt/Programs/Sundials/cvodes-2.9.0-inst/include -L/home/knt/Programs/Sundials/cvodes-2.9.0-inst/lib -mtune=core2 -std=c++11"
+    PKG_LIBS="-I/home/knt/Programs/Sundials/cvodes-2.9.0-inst/include -L/home/knt/Programs/Sundials/cvodes-2.9.0-inst/lib  -lsundials_cvodes -lsundials_nvecserial -Wl,-rpath,/home/knt/Programs/Sundials/cvodes-2.9.0-inst/lib -pthread"
+  }
 } else if(Sys.info()[["sysname"]] == "Windows" ){
   print("NOT WORKING !!!")
   break()
