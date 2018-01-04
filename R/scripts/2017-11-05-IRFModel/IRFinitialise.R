d4d5b0908b4e83ecd2c25cb811f29be2f2ae1b30
stork119 <karol.nienaltowski@gmail.com>
2018-01-04 15:29:53 +0100

    Organising scripts

--- R/scripts/2017-11-05-IRFModel/IRFinitialise.R
+++ R/scripts/2017-11-05-IRFModel/IRFinitialise.R
@@ -2,14 +2,14 @@
 ### IRF library 
 ###
 # setwd("../../../")
-source("R/optimisation/initialise_optimisation.R")
+# source("R/optimisation/initialise_optimisation.R")
 #source("R/scripts/2017-11-05-IRFModel/IRFlibrary.R")
-library(e1071)
-library(CapacityLogReg)
+source("R/libraries/library_data_manipulation.R")
+source("R/graphics/libraries.R")
 
 #### read input ####
 poster.path.list <- list()
-poster.path.list$input.dir <- "resources/input/poster/"
+poster.path.list$input.dir <- "resources/input/poster/2017-12-18/rds/"
 
 poster.path.list$output.dir <- "resources/output/poster/"
 
@@ -17,11 +17,13 @@ irfmodel.path.list <- list()
 irfmodel.path.list$output.dir <- "resources/output/IRFmodel"
 #dir.create(irfmodel.path.list$output.dir, recursive = TRUE)
 
-irfmodel.path.list$rds.path <- "/home/knt/Documents/modelling/resources/input/poster/data_ffc_joined.RDS"
+irfmodel.path.list$rds.path <- "/home/knt/Documents/modelling/resources/input/poster/data_ffc_filtered.RDS"
 poster.data.list <- readRDS(file = irfmodel.path.list$rds.path)
+
 source("R/scripts/2017-11-05-IRFModel/IRFdata.R")
 source("R/scripts/2017-11-05-IRFModel/IRFmodel_irf1.R")
 source("R/scripts/2017-11-05-IRFModel/IRFmodel_ps1.R")
 source("R/scripts/2017-11-05-IRFModel/IRFoptimise_fun.R")
+source("R/scripts/2017-11-05-IRFModel/IRFsave.R")
 
 setwd(dir = "knt/Documents/modelling/")
